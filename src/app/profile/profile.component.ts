fa61484d79eb0acc78f7fa54c0a29835bec1055d
Alireza <alireza.lotfi@techclass.com>
2022-07-17 11:33:02 +0430

    done

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProfileRemoteService} from "../remote/profile-remote.service";
+import {ActivatedRoute} from "@angular/router";
+import {PostDto, ProfileDto, ProfileInput} from "../remote/dto";
+import {AuthService} from "../../auth/auth.service";
+import {PostRemoteService} from "../remote/post-remote.service";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile: ProfileDto;
+  posts: PostDto[];
 
-  ngOnInit(): void {
+  constructor(
+    private profileRemote: ProfileRemoteService,
+    public authService: AuthService,
+    private postService: PostRemoteService,
+    private activatedRoute: ActivatedRoute
+  ) {
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((x: any) => {
+      if (x.id) {
+        this.getProfile(x.id)
+        this.getUserPosts(x.id)
+      }
+    })
+  }
+
+  getProfile(id: number) {
+    this.profileRemote.getProfile(id).subscribe(x => {
+      this.profile = x.result
+    })
+  }
+
+  getUserPosts(id: number) {
+    this.postService.getPostByUserId(id).subscribe(x => {
+      this.posts = x.result
+    })
+  }
+
+  upload(e: any) {
+    this.profileRemote.upload({
+      data: e.target.files[0],
+      fileName: e.target.files[0].name
+    }).subscribe(x => {
+      let updateInput = new ProfileInput()
+      updateInput.image = x.result
+      updateInput.name = this.profile.user.name
+      this.profileRemote.updateProfile(updateInput).subscribe(z => {
+        location.reload()
+      })
+    })
+  }
 }
--- src/app/remote/profile-remote.service.ts
+++ src/app/remote/profile-remote.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {FileParameter, ProfileDto} from "./dto";
+import {FileParameter, LikeInput, ProfileDto, ProfileInput, UserDto} from "./dto";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
 import BaseResult from "./BaseResult";
@@ -20,7 +20,7 @@ export class ProfileRemoteService {
   }
 
   upload(file: FileParameter | undefined): Observable<BaseResult<string>> {
-    let url_ = environment.apiUrl  + "/api/services/app/Profile/Upload";
+    let url_ = environment.apiUrl + "/api/services/app/Profile/Upload";
     url_ = url_.replace(/[?&]$/, "");
 
     const content_ = new FormData();
@@ -29,8 +29,70 @@ export class ProfileRemoteService {
     else
       content_.append("file", file.data, file.fileName ? file.fileName : "file");
     console.log(file)
-    return this.http.put<any>(url_,content_);
+    return this.http.put<any>(url_, content_);
   }
+
+  updateProfile(body: ProfileInput | undefined): Observable<BaseResult<void>> {
+    let url_ = environment.apiUrl + "/api/services/app/Profile/UpdateProfile";
+    return this.http.put<any>(url_, body);
+  }
+
+  /**
+   * @param id (optional)
+   * @return Success
+   */
+  getProfile(id: number | undefined): Observable<BaseResult<ProfileDto>> {
+    let url_ = environment.apiUrl + "/api/services/app/Profile/GetProfile?";
+    if (id === null)
+      throw new Error("The parameter 'id' cannot be null.");
+    else if (id !== undefined)
+      url_ += "Id=" + encodeURIComponent("" + id) + "&";
+    url_ = url_.replace(/[?&]$/, "");
+    return this.http.get<any>(url_);
+  }
+
+
+  /**
+   * @param body (optional)
+   * @return Success
+   */
+  follow(body: LikeInput | undefined): Observable<void> {
+    let url_ = environment.apiUrl + "/api/services/app/Profile/Follow";
+    url_ = url_.replace(/[?&]$/, "");
+    return this.http.post<any>(url_, body);
+  }
+
+
+  /**
+   * @param id (optional)
+   * @return Success
+   */
+  getFollowers(id: number | undefined): Observable<BaseResult<UserDto[]>> {
+    let url_ = environment.apiUrl + "/api/services/app/Profile/GetFollowers?";
+    if (id === null)
+      throw new Error("The parameter 'id' cannot be null.");
+    else if (id !== undefined)
+      url_ += "Id=" + encodeURIComponent("" + id) + "&";
+    url_ = url_.replace(/[?&]$/, "");
+    return this.http.get<any>(url_);
+
+  }
+
+
+  /**
+   * @param id (optional)
+   * @return Success
+   */
+  getFollowings(id: number | undefined): Observable<BaseResult<UserDto[]>> {
+    let url_ = environment.apiUrl + "/api/services/app/Profile/GetFollowings?";
+    if (id === null)
+      throw new Error("The parameter 'id' cannot be null.");
+    else if (id !== undefined)
+      url_ += "Id=" + encodeURIComponent("" + id) + "&";
+    url_ = url_.replace(/[?&]$/, "");
+    return this.http.get<any>(url_);
+  }
+
 
 }
 
